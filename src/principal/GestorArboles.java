ccd727c562c483eb49822f5b2b08329065149f07
plaiaundi <plaiaundi@HZ309807.H012108.NET>
2023-01-23 13:46:33 +0100

    PreparedStatemen hecho / Statement comentado*

--- src/principal/GestorArboles.java
+++ src/principal/GestorArboles.java
@@ -2,6 +2,7 @@ package principal;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,6 +10,11 @@ import java.util.Scanner;
 
 import clases.Arbol;
 
+/**
+ * 
+ * @author ${Arnold Bermell}
+ *
+ */
 public class GestorArboles {
 
 	private static final String host = "localhost";
@@ -50,6 +56,10 @@ public class GestorArboles {
 	            System.out.println(SALIR + ". Salir");
 	            conexion = DriverManager.getConnection("jdbc:mysql://" + host + "/" + bbdd, username, password);
 				Statement st = conexion.createStatement();
+				
+				
+				//PREPARATE
+				
 				opcion_menu = scan.nextInt();
 				scan.nextLine();
 	            switch (opcion_menu) {
@@ -64,16 +74,27 @@ public class GestorArboles {
 	                origen = scan.nextLine();
 	                System.out.println("Introduzca la altura: " );
 	                altura = scan.nextInt();
-	                st.execute("INSERT INTO arbol (nombre_comun, nombre_cientifico, habitat, altura, origen) VALUES ('"+nombreComun+"', '"+nombreCientifico+"', '"+habitat+"', "+altura+", '"+origen+"');");
-	                conexion.close();
-	                st.close();
+	                
+	                PreparedStatement ps = conexion.prepareStatement("INSERT INTO arbol (nombre_comun, nombre_cientifico, habitat, altura, origen) VALUES (?, ?, ? ,?, ?);");
+	                ps.setString(1, nombreComun);
+	                ps.setString(2, nombreCientifico);
+	                ps.setString(3,  habitat);
+	                ps.setInt(4, altura);
+	                ps.setString(5, origen);
+	                ps.execute();
+//	                st.execute("INSERT INTO arbol (nombre_comun, nombre_cientifico, habitat, altura, origen) VALUES ('"+nombreComun+"', '"+nombreCientifico+"', '"+habitat+"', "+altura+", '"+origen+"');");
+//	                conexion.close();
+//	                st.close();
 	                //INSERT INTO `arbol` (`id`, `nombre_comun`, `nombre_cientifico`, `habitat`, `altura`, `origen`) VALUES (NULL, 'roble', 'robledo', 'jungla', '2', 'Habitacion');
 	                break;
 	            case OPCION_DOS:
 	                System.out.println("Introduzca la ID del arbol que desee eliminar: \n");
 	                id = scan.nextInt();
-	                String sentenciaDelete = "DELETE FROM arbol WHERE `arbol`.`id` = "+id;
-	                st.execute(sentenciaDelete);
+	                ps = conexion.prepareStatement("DELETE FROM arbol WHERE `arbol`.`id` = ?");
+	                ps.setInt(1, id);
+	                ps.execute();
+//	                String sentenciaDelete = "DELETE FROM arbol WHERE `arbol`.`id` = "+id;
+//	                st.execute(sentenciaDelete);
 	                
 	                //"DELETE FROM arbol WHERE `arbol`.`id` = 2"
 	                break;
@@ -100,8 +121,13 @@ public class GestorArboles {
 	            		System.out.println("Introduzca el nuevo valor: " );
 			            String nuevoValor = scan.nextLine();
 			            
-			            String sentenciaActualizar = "UPDATE arbol SET `"+modificar+"` = '"+nuevoValor+"' WHERE `arbol`.`id` = "+id;
-			            st.execute(sentenciaActualizar);
+//			            String sentenciaActualizar = "UPDATE arbol SET `"+modificar+"` = '"+nuevoValor+"' WHERE `arbol`.`id` = "+id;
+			            //UPDATE arboles SET nombre_comun= ?, nombre_cientifico= ?, habitad= ?, altura= ?, origen= ? WHERE id = ?;
+			            ps = conexion.prepareStatement("UPDATE arbol SET " + modificar + " = ? WHERE id = ?");
+			            ps.setString(1, nuevoValor);
+			            ps.setInt(2, id);
+			            ps.execute();
+//			            st.execute(sentenciaActualizar);
 	            	}
 	            	else {
 	            		System.out.println("Introduzca un valor valido");
@@ -109,15 +135,15 @@ public class GestorArboles {
 	            	}
 	            		break;
 	            	
-		            	
-	            	
-	            	
 	            	//UPDATE `arbol` SET `nombre_comun` = 'siso' WHERE `arbol`.`id` = 5
 	            	
 
 	            case OPCION_CUATRO:
-	            	String visualizarArboles = "SELECT * FROM arbol";
-	            	ResultSet resultado = st.executeQuery(visualizarArboles);
+//	            	String visualizarArboles = "SELECT * FROM arbol";
+//	            	ResultSet resultado = st.executeQuery(visualizarArboles);
+	            	ps = conexion.prepareStatement("SELECT * FROM arbol");
+//	              	ResultSet resultado = ps.executeQuery(visualizarArboles);
+	              	ResultSet resultado = ps.executeQuery();
 	            	System.out.println("ID | NOMBRE COMUN | NOMBRE CIENTIFICO | HABITAT | ALTURA | ORIGEN");
 	            	while(resultado.next()) {
 	            		System.out.println(resultado.getInt(1)+" | "+resultado.getString(2)+" | "+resultado.getString(3)+" | "+resultado.getString(4)+" | "+resultado.getString(5)+" | "+resultado.getString(6));
@@ -135,7 +161,7 @@ public class GestorArboles {
 	        } while (opcion_menu != SALIR);
 	        
 	        conexion.close();
-	        
+	        scan.close();
 	        
 		}
 		
